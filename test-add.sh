65f415770dd9b6d363e90f44326f2209a826f29b
Elliot <elliotblanch@gmail.com>
2022-04-28 14:50:55 +0100

    actually fixed bugs in test-add

--- test-add.sh
+++ test-add.sh
@@ -4,13 +4,13 @@ declare -a num2s=(3 44 15 26 30 1)
 declare -A results=()
 for i in {0..5}; do
   a=${num1s[$i]}
-  b=${num2s{$i]}
+  b=${num2s[$i]}
   results[$a,$b]=$(($a + $b))
 done
 
 for i in {0..5}; do
   a=${num1s[$i]}
-  b=${num2s{$i]}
+  b=${num2s[$i]}
   if [ $(./add.sh $a $b) -eq ${results[$a,$b]} ]; then
     echo "test passed"
   else
